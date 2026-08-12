1eaffe7ec7186599f2515ef61e4595778674b5c9
Ghostvulture <xluo799@connect.hkust-gz.edu.cn>
2026-08-12 13:06:21 +0800

    protocol: ensure 1200 nonoverlapping model-test units

--- tests/test_natural_ppo_state_risk_protocol.py
+++ tests/test_natural_ppo_state_risk_protocol.py
@@ -17,6 +17,10 @@ def test_natural_ppo_state_risk_protocol_has_disjoint_sac_roles():
     assert all(seeds[i].isdisjoint(seeds[j])
                for i in range(len(seeds)) for j in range(i + 1, len(seeds)))
     assert roles["checkpoint_steps"] == [25000, 50000, 100000]
+    protected = roles["protected_model_test"]
+    assert protected["exposure_policy_steps_per_source"] == 15000
+    assert protected["minimum_snapshot_spacing_policy_steps"] > protocol[
+        "target"]["risk_horizon_policy_steps"]
     assert protocol["target"]["fall_predicate"] == {
         "minimum_base_link_origin_height_m": 0.18,
         "maximum_abs_roll_or_pitch_rad": 1.047198,
